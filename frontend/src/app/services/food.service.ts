0129a67e43b6b500f3b3b2e7031bd46b3f6fd83d
Wayd26 <wayedoun@gmail.com>
2023-03-13 11:11:55 +0100

    add Method for search in Food Service

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -10,7 +10,11 @@ export class FoodService {
   constructor() { }
 
 
-  getAll( ): Food[] {
+  getAll(): Food[] {
     return sample_foods;
+  }
+
+  getAllFoodsBySearchTerm(searchTerm: string) {
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
   }
 }
